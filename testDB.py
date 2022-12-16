c88284a259dafe39a45416c655e3628b84d2562b
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2022-12-16 13:04:26 +0100

    Aggiunta codice SQL e pyton tester per il db

---
+++ testDB.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import mysql.connector
+from mysql.connector import errorcode
+
+print("Hello world!")
+try:
+    mydb = mysql.connector.connect(user='2YLoomalqj', 
+        password='8lF7C5A1MT', 
+        host='remotemysql.com', 
+        database='2YLoomalqj')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM USERS")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+
+#print(mydb)
